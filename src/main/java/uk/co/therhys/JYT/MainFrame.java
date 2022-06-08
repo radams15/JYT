eac547e52ed26bf1051650e824b81dda45f1de00
Rhys Adams (Home) <rhys@therhys.co.uk>
2022-06-08 15:39:42 +0100

    Working with swt on multiple architectures and operating systems with maven profiles.

--- src/main/java/uk/co/therhys/JYT/MainFrame.java
+++ src/main/java/uk/co/therhys/JYT/MainFrame.java
@@ -1,99 +1,140 @@
 package uk.co.therhys.JYT;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+import uk.co.therhys.YT.Channel;
 import uk.co.therhys.YT.Config;
 import uk.co.therhys.YT.Video;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.Date;
 
-public class MainFrame extends JFrame {
+public class MainFrame {
     private Config conf;
+    private Display display;
+    private Shell shell;
 
-    private DefaultTableModel tableModel;
-    private JTable table;
+    private Table table;
 
-    private ActionListener playListener;
+    private SelectionListener playListener;
 
-    private static JButton newToolButton(String title, String img, ActionListener listener){
-        JButton btn = new JButton(title);
+    private ToolItem newToolButton(ToolBar bar, String title, String img, SelectionListener listener){
+        ToolItem btn = new ToolItem(bar, SWT.PUSH);
+        btn.setText(title);
 
-        ImageIcon icon;
+        Image icon;
         try{
             URL url = ClassLoader.getSystemClassLoader().getResource(img);
-            icon = new ImageIcon(url);
+            icon = new Image(display, url.openStream());
         }catch(Exception e){
-            icon = new ImageIcon("src/main/resources/"+img);
+            icon = new Image(display, "src/main/resources/"+img);
         }
 
-        btn.setIcon(icon);
+        btn.setImage(icon);
 
-        btn.addActionListener(listener);
+        btn.addSelectionListener(listener);
 
         return btn;
     }
 
     private void setupActions(){
-        playListener = new ActionListener() {
-            public void actionPerformed(ActionEvent actionEvent) {
-                int row = table.getSelectedRow();
-
-                Video vid = (Video) tableModel.getValueAt(row, 3);
+        playListener = new SelectionListener() {
+            public void widgetSelected(SelectionEvent selectionEvent) {
+                TableItem[] items = table.getSelection();
+                if(items.length != 1){
+                    return;
+                }
+                Video vid = (Video) items[0].getData();
 
                 System.out.println(vid.title);
+            }
+
+            public void widgetDefaultSelected(SelectionEvent selectionEvent) {
+
             }
         };
     }
 
     void setupToolbar(){
-        JToolBar toolbar = new JToolBar();
+        ToolBar toolbar = shell.getToolBar();
+        if(toolbar == null){
+            toolbar = new ToolBar(shell, SWT.HORIZONTAL);
+        }
+        toolbar.setSize(300, 70);
+        toolbar.setLocation(0, 0);
 
-        toolbar.add(newToolButton("Play", "playpause.png", playListener));
-
-        getContentPane().add(toolbar, BorderLayout.NORTH);
+        newToolButton(toolbar, "Play", "playpause.png", playListener);
     }
 
     void setupMenubar(){
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
+        Menu menuBar = shell.getMenu();
+        if(menuBar == null){
+            menuBar = new Menu(shell, SWT.BAR);
+        }
 
-        JMenu videoMenu = new JMenu("Video");
-        menuBar.add(videoMenu);
+        MenuItem video = new MenuItem(menuBar, SWT.CASCADE);
+        video.setText("Video");
+        Menu videoMenu = new Menu(shell, SWT.DROP_DOWN);
+        video.setMenu(videoMenu);
 
-        videoMenu.add("Play").addActionListener(playListener);
+        MenuItem playMenu = new MenuItem(videoMenu, SWT.PUSH);
+        playMenu.setText("Play\tCTRL+P");
+        playMenu.setAccelerator(SWT.CTRL+'P');
+        playMenu.addSelectionListener(playListener);
+
+        shell.setMenuBar(menuBar);
     }
 
     private void setupUI(){
-        setSize(800, 600);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
-        BorderLayout layout = new BorderLayout();
-        getContentPane().setLayout(layout);
-
-        table = new JTable(new DefaultTableModel(new Object[]{"Title", "Channel", "Published", "Obj"}, 0));
-        JScrollPane scrollPane = new JScrollPane(table);
-        getContentPane().add(scrollPane, BorderLayout.CENTER);
-        tableModel = (DefaultTableModel) table.getModel();
-
-        table.getColumnModel().removeColumn(table.getColumnModel().getColumn(3));
+        shell.setSize(800, 600);
+        shell.setLayout(new GridLayout(1, true));
 
         setupActions();
         setupToolbar();
         setupMenubar();
+
+        table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.RESIZE | SWT.V_SCROLL | SWT.H_SCROLL);
+        table.setLinesVisible(true);
+        table.setHeaderVisible(true);
+
+        String[] titles = new String[]{"Title", "Channel", "Published"};
+        for(int i=0 ; i<titles.length ; i++){
+            new TableColumn(table, SWT.NONE).setText(titles[i]);
+        }
     }
 
     public MainFrame(Config conf){
+        display = new Display();
+        shell = new Shell(display);
+
         this.conf = conf;
 
         setupUI();
 
-        for(Video vid : conf.getVideos()){
-            tableModel.addRow(new Object[]{vid.title, vid.channel.getName(), new Date((long)vid.published*1000), vid});
+        for(Video vid : conf.subscriptions.get(0).getVideos(conf)){
+            TableItem itm = new TableItem(table, SWT.NONE);
+            itm.setText(0, vid.title);
+            itm.setText(1, vid.channel.getName());
+            itm.setText(2, new Date((long)vid.published*1000).toString());
+            itm.setData(vid);
         }
+        for(int i=0 ; i<3 ; i++){
+            table.getColumn(i).pack();
+        }
+
+
+        shell.pack();
+        shell.open();
+
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+        display.dispose();
     }
 }
--- src/main/java/uk/co/therhys/YT/Net.java
+++ src/main/java/uk/co/therhys/YT/Net.java
@@ -1,13 +1,52 @@
 package uk.co.therhys.YT;
 
+import javax.net.ssl.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.Socket;
 import java.net.URL;
+import java.net.URLConnection;
+import java.security.*;
+import java.security.cert.X509Certificate;
 
 public class Net {
-    public String get(String requestURL){
+
+    private SSLSocketFactory getTrustAllFactory() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509ExtendedTrustManager() {
+                    public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket) {}
+                    public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) {}
+                    public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine) {}
+                    public void checkServerTrusted(X509Certificate[] chain, String authType, SSLEngine engine) {}
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, null);
+
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+        return sslContext.getSocketFactory();
+    }
+
+    private String httpUrlGet(String requestURL){
+                /*try {
+            System.out.println("A");
+            HttpsURLConnection.setDefaultSSLSocketFactory(getTrustAllFactory());
+            System.out.println("B");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }*/
         HttpURLConnection connection = null;
 
         try {
@@ -20,7 +59,6 @@ public class Net {
             connection.setUseCaches(false);
             connection.setDoOutput(true);
 
-            //Get Response
             InputStream is = connection.getInputStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is));
             StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
@@ -39,5 +77,58 @@ public class Net {
                 connection.disconnect();
             }
         }
+    }
+
+    private String urlGet(String requestURL){
+        try {
+            URL url = new URL(requestURL);
+            URLConnection conn = url.openConnection();
+            // fake request coming from browser
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB;     rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+            StringBuilder out = new StringBuilder();
+            String f;
+            while(true) {
+                f = in.readLine();
+                if(f == null) break;
+                out.append(f);
+            }
+
+            in.close();
+
+            return out.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "Error!";
+    }
+
+    private String curlGet(String requestURL){
+        try {
+            String[] commands = new String[] {
+                "curl", "-k", "-X", "GET", requestURL
+            };
+
+            Process process = Runtime.getRuntime().exec(commands);
+            BufferedReader reader = new BufferedReader(new
+                    InputStreamReader(process.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            return response.toString();
+
+        }catch(IOException e){
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public String get(String requestURL){
+        return urlGet(requestURL);
     }
 }
